e56311d56272a057709d5e155808d2322805ca77
Mihail <trotm96@mail.ru>
2022-02-12 17:05:29 +0300

    done.grammar mistake

--- src/main/java/tms/instaclone/web/servlet/user/AuthorizationServletNoImage.java
+++ src/main/java/tms/instaclone/web/servlet/user/AuthorizationServletNoImage.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/accounts/login/")
-public class AutorizationServletNoImage extends HttpServlet {
+public class AuthorizationServletNoImage extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getServletContext().getRequestDispatcher("/page/user/authorizationNoImage.jsp").forward(req, resp);
