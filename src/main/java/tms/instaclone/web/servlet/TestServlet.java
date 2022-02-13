06f7958026e435bffff442bf55993e0029710194
gemuz_home <gemuzkm@gmail.com>
2022-02-13 14:19:03 +0300

    test config

--- src/main/java/tms/instaclone/web/servlet/TestServlet.java
+++ src/main/java/tms/instaclone/web/servlet/TestServlet.java
@@ -1,4 +1,4 @@
-package tms.instaclone;
+package tms.instaclone.web.servlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,10 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/")
-public class Main extends HttpServlet {
+@WebServlet(urlPatterns = "/test")
+public class TestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getServletContext().getRequestDispatcher("/page/index.jsp").forward(req, resp);
+        req.getServletContext().getRequestDispatcher("/page/test.jsp").forward(req, resp);
     }
 }
---
+++ src/main/webapp/page/test.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: gemuz
+  Date: 13.02.2022
+  Time: 14:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Test page</title>
+</head>
+<body>
+
+<img src="/page/images/test.jpeg">
+
+</body>
+</html>
