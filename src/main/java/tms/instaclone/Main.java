9a7e21875346318e935e3b84fa1bdfbc4e2726cd
gemuz_home <gemuzkm@gmail.com>
2022-02-13 14:21:41 +0300

    done web access

--- src/main/java/tms/instaclone/Main.java
+++ src/main/java/tms/instaclone/Main.java
@@ -1,16 +1,5 @@
 package tms.instaclone;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+public class Main {
 
-@WebServlet(urlPatterns = "/")
-public class Main extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getServletContext().getRequestDispatcher("/page/index.jsp").forward(req, resp);
-    }
 }
