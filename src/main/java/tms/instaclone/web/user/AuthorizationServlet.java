b99ade69219ee4d87a54000f203163d812a79c0c
papaslip <kuchuk.sa17@gmail.com>
2022-02-17 22:40:46 +0300

    add method Post in AuthorizationServletNoImage.java and remade authorizationNoImage.jsp

--- src/main/java/tms/instaclone/web/user/AuthorizationServlet.java
+++ src/main/java/tms/instaclone/web/user/AuthorizationServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/")
+@WebServlet(urlPatterns = "/", name = "AuthorizationServlet")
 public class AuthorizationServlet extends HttpServlet {
 
     @Override
