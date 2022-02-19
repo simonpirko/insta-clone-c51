5bbb1defc2ec1f01dd189a9091f1ee9816d223ab
Larisa <larisashkvarko1@gmail.com>
2022-02-19 19:58:22 +0300

    user page

--- src/main/java/tms/instaclone/web/user/Constants.java
+++ src/main/java/tms/instaclone/web/user/Constants.java
@@ -6,4 +6,5 @@ public final class Constants {
   
     public static final String REGISTRATION_JSP = "/page/user/registration.jsp";
     public static final String AUTHORIZATION_JSP = "/page/user/authorization.jsp";
+    public static final String USER_PAGE_JSP = "/page/user/homePage/userPage.jsp";
 }
