5ff1fb6aecec6461fb972fb21b14b058240f75e3
gemuzkm <gemuzkm@gmail.com>
2022-02-28 22:11:18 +0300

    fix: upload project path

--- src/main/java/tms/instaclone/web/servlet/post/addPostServlet.java
+++ src/main/java/tms/instaclone/web/servlet/post/addPostServlet.java
@@ -1,13 +1,21 @@
 package tms.instaclone.web.servlet.post;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
+import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
 @WebServlet(urlPatterns = "/addpost")
 @MultipartConfig(
@@ -18,10 +26,38 @@ import java.io.IOException;
 public class addPostServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Part filePart = req.getPart("file");
-        String fileName = filePart.getSubmittedFileName();
-        for (Part part : req.getParts()) {
-            part.write("C:\\Users\\User\\Desktop\\" + fileName);
+
+        boolean isMultipart = ServletFileUpload.isMultipartContent(req);
+        if (isMultipart) {
+            // Create a factory for disk-based file items
+            FileItemFactory factory = new DiskFileItemFactory();
+            // Create a new file upload handler
+            ServletFileUpload upload = new ServletFileUpload(factory);
+            try {
+                // Parse the request
+                List /* FileItem */ items = upload.parseRequest(req);
+                Iterator iterator = items.iterator();
+                while (iterator.hasNext()) {
+                    FileItem item = (FileItem) iterator.next();
+                    if (!item.isFormField()) {
+                        String fileName = item.getName();
+                        String root = getServletContext().getRealPath("/src/main/webapp/");
+                        root = root.replace("insta-clone-c51\\target\\", "");
+                        File path = new File(root + "/user_data_storage");
+                        if (!path.exists()) {
+                            boolean status = path.mkdirs();
+                        }
+                        File uploadedFile = new File(path + "/" + fileName);
+                        System.out.println(uploadedFile.getAbsolutePath());
+                        item.write(uploadedFile);
+                    }
+                }
+
+            } catch (FileUploadException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
